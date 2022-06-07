fb9a68d331ecb2fbde22ff7783869b7f2b62a2ad
rexdragin59 <timesphere2@gmail.com>
2022-06-06 22:46:10 -0700

    Delete index.php

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-	header("Access-Control-Allow-Origin: *");	// enable cross-origin
-	include 'circleDefense.html';	// run contents of index html
-	// open this file via localhost/... after turning on XAMPP server
-?>
